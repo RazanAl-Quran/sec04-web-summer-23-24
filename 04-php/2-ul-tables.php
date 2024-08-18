d0c7b2a9c028a8eecae5686378a15acae633c5c2
RazanAl-Quran <quraan_razan@hotmail.com>
2024-08-19 02:16:33 +0300

    feat: php-02 class has been added

---
+++ 04-php/2-ul-tables.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h3>Your course are:</h3>
+    <?php
+    // send a request from the web server to our DB server in order to get all the courses 
+    $courses = array('programming', 'advanced programming', 'data structures');
+    echo "<ul>";
+    for ($i = 0; $i < count($courses); $i++) {
+        echo "<li>$courses[$i]</li>";
+    }
+    echo "</ul>";
+
+    echo phpversion();
+
+    // TABLES
+    $grades = array("khaled" => 80, "Saif" => 100, "rana" => 98);
+    echo "<table class='grades-table' border='1'>";
+    echo "
+    <tr>
+        <th>Student</th>
+        <th>Grade</th>
+    </tr>
+    ";
+    foreach ($grades as $student => $grade) {
+        echo "
+                <tr>
+                <td>$student</td>
+                <td>$grade</td>
+                </tr>
+        ";
+
+    }
+
+    echo "</table>";
+    ?>
+
+</body>
+
+</html>
