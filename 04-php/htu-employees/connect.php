b78f75c503f495813038db926cf73e2c397cfff1
RazanAl-Quran <quraan_razan@hotmail.com>
2024-08-21 19:22:42 +0300

    feat: htu-employee demo php-05 class has been added

---
+++ 04-php/htu-employees/connect.php
@@ -0,0 +1,13 @@
+<?php
+
+$con = new mysqli('localhost','root','','employees6');
+
+// if($con) {
+//     echo 'success connection';
+// } else {
+//     echo 'failed connection';
+//     mysqli_error($con);
+// }
+
+
+?>
---
+++ 04-php/htu-employees/deleteUser.php
@@ -0,0 +1,24 @@
+<?php
+
+include 'connect.php';
+
+
+// http://localhost/5-PHP/7-PHP-form-Employees/deleteUser.php?id=26
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $sql = "DELETE FROM users WHERE id = $id;";
+
+    $result = mysqli_query($con, $sql);
+
+    if ($result) {
+        echo "data deleted successfully";
+        header("Location:display.php");
+    } else {
+        echo "data deleted unsuccessfully";
+        mysqli_error($con);
+    } 
+
+}
+
+?>
